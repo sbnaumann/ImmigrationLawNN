c693deed646db18f1542134edb8f350af846eddd
Samuel Naumann <snaumann2@gmail.com>
2020-12-30 18:23:19 -0500

    (Learning) Added a second Button

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title('Title')
+root.geometry("400x400")
+
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+    e.delete(0, 'end')
+    myLabel.pack(pady=10)
+
+e = Entry(root, width=50, font=('Helvetica', 30))
+e.pack(padx=10, pady=10)
+
+button = ttk.Button(root, text="Click Me")
+button.pack()
+
+myButton = Button(root, text="enter your name please", command=myClick)
+myButton.pack(pady=10)
+
+root.mainloop()
